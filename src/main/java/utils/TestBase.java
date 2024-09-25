5d5bef57ba8663828c2e3270ff326b8a613ffe37
IvanaJoshevska96 <ivana.josevska74@gmail.com>
2024-09-25 22:46:17 +0200

    added methods for switching windows

--- src/main/java/utils/TestBase.java
+++ src/main/java/utils/TestBase.java
@@ -2,12 +2,16 @@ package utils;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
 
 @ExtendWith(TestWatcherExtension.class)
 public class TestBase {
     protected static WebDriver driver;
+    private static String originalWindow;
 
     public static void setup() {
 
@@ -23,6 +27,21 @@ public class TestBase {
             driver.quit();
             System.out.println("ChromeDriver session ended");
         }
+    }
+
+    public static void switchToNewWindow() {
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String windowHandle : windowHandles) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                return;
+            }
+        }
+        throw new NoSuchWindowException("No new window found to switch to.");
+    }
+
+    public void switchToOriginalWindow() {
+        driver.switchTo().window(originalWindow);
     }
 }
 
